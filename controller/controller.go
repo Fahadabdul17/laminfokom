c1518ba614e08f2e268b688ba98d6b272aabed2b
fahad17 <fahadra96@gmail.com>
2023-07-17 22:16:14 +0700

    1

--- controller/controller.go
+++ controller/controller.go
@@ -1,24 +1,17 @@
 package controller
 
 import (
-	"iteung/config"
-
+	"github.com/aiteung/musik"
+	"github.com/fahadabdul17/hadbackend"
+	"github.com/fahadabdul17/laminfokom/config"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	"github.com/whatsauth/whatsauth"
 )
 
-type HTTPRequest struct {
-	Header string `json:"header"`
-	Body   string `json:"body"`
-}
-
-func Sink(c *fiber.Ctx) error {
-	var req HTTPRequest
-	req.Header = string(c.Request().Header.Header())
-	req.Body = string(c.Request().Body())
-	return c.JSON(req)
-}
+var DataAkreditas = "dataakreditas"
+var DataProgramStudi = "dataprogramstudi"
+var Profile = "profile"
 
 func WsWhatsAuthQR(c *websocket.Conn) {
 	whatsauth.RunSocket(c, config.PublicKey, config.Usertables[:], config.Ulbimariaconn)
@@ -39,4 +32,24 @@ func PostWhatsAuthRequest(c *fiber.Ctx) error {
 		return c.JSON(ws)
 	}
 
+}
+
+func Homepage(c *fiber.Ctx) error {
+	ipaddr := musik.GetIPaddress()
+	return c.JSON(ipaddr)
+}
+
+func GetDataAkreditas(c *fiber.Ctx) error {
+	getstatus := hadbackend.GetDataAkreditas("Masih Berlaku")
+	return c.JSON(getstatus)
+}
+
+func GetDataProgramStudi(c *fiber.Ctx) error {
+	getstatus := hadbackend.GetDataProgramStudi("Sarjana Terapan")
+	return c.JSON(getstatus)
+}
+
+func GetProfile(c *fiber.Ctx) error {
+	getstatus := hadbackend.GetDataProfile("kampus merdeka")
+	return c.JSON(getstatus)
 }
--- url/url.go
+++ url/url.go
@@ -1,7 +1,7 @@
 package url
 
 import (
-	"iteung/controller"
+	"github.com/fahadabdul17/laminfokom/controller"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
@@ -10,12 +10,9 @@ import (
 func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
-
-	page.Get("/", controller.Sink)
-	page.Post("/", controller.Sink)
-	page.Put("/", controller.Sink)
-	page.Patch("/", controller.Sink)
-	page.Delete("/", controller.Sink)
-	page.Options("/", controller.Sink)
-
+	page.Get("/", controller.Homepage)              //API from user whatsapp message from iteung gowa
+	page.Get("/dataakreditas", controller.GetDataAkreditas) //API from user whatsapp message from iteung gowa
+	page.Get("/dataprogramstudi", controller.GetDataProgramStudi)   //API from user whatsapp message from iteung gowa
+	page.Get("/profile", controller.GetProfile)   //API from user whatsapp message from iteung gowa
+	
 }
